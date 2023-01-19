be13540f3edf746fc1c6d555173b2067813171ba
ABDULKAFI WALEED ABDULKAFI AL MALAMI <1201102665@student.mmu.edu.my>
2023-01-19 17:57:05 +0800

    Add info

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
 // Lab: TxxL 
-// Names: MEMBER_NAME_1 | MEMBER_NAME_2 | MEMBER_NAME_3 
-// IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 
-// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
-// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// Names: MEMBER_NAME_1 | ABDULKAFI WALEED ABDULKAFI AL MALAMI | MEMBER_NAME_3 
+// IDs: MEMBER_ID_1 | 1201102665| MEMBER_ID_3 
+// Emails: MEMBER_EMAIL_1 | 1201102665@student.mmu.edu.my | MEMBER_EMAIL_3 
+// Phones: MEMBER_PHONE_1 | +60183211296 | MEMBER_PHONE_3 
 // ********************************************************* 
 
 // TODO: Fill in the missing information above and delete this line.
