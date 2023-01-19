de87ae09099720e9806e66547da38cb5ea479551
Douaa MOALI <1211307919@student.mmu.edu.my>
2023-01-19 17:55:00 +0800

    add info

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 // ********************************************************* 
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
-// Lab: TxxL 
-// Names: MEMBER_NAME_1 | MEMBER_NAME_2 | MEMBER_NAME_3 
-// IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 
-// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
-// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// Lab: TT8L 
+// Names: MEMBER_NAME_1 | MEMBER_NAME_2 | Douaa_moali_3 
+// IDs: MEMBER_ID_1 | MEMBER_ID_2 | 1211307919_3 
+// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | 1211307919@student.mmu.edu.myL_3 
+// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | 01825653383_3 
 // ********************************************************* 
 
 // TODO: Fill in the missing information above and delete this line.
