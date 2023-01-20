620dc7325ce03a5e47b758ab57ba9991bbf7c7e3
ABDULKAFI WALEED <123051114+1201102665@users.noreply.github.com>
2023-01-20 10:42:01 +0800

    Ask user input for default settings (#3)
    * Ask user input for default settings
    
    * Ask user input for default settings

--- main.cpp
+++ main.cpp
@@ -10,13 +10,75 @@
 
 // TODO: Fill in the missing information above and delete this line.
 
-#include "pf/helper.h"
+//#include "pf/helper.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
+
     cout << "Assignment (Part 1)" << endl;
     cout << "Let's Get Started!" << endl;
-    pf::Pause();
+
+    int BoardRows = 5;
+    int BoardColumns = 9;
+    int Zombie = 1;
+    char changeSettings;
+    cout << " --_--------------" << endl;
+    cout << "  = Default Game Settings! =" << endl;
+    cout << " _--------------_-" << endl;
+    cout << "Board Rows : " << BoardRows << endl;
+    cout << "Board Columns: " << BoardColumns << endl;
+    cout << "Zombie Count: " << Zombie  << endl;
+    cout << "Do you wish to change the game default settings (y/n)? ";
+    cin >> changeSettings;
+
+while (changeSettings == 'y' ||changeSettings == 'Y') {
+    int input;
+    cout << "Enter Rows: ";
+    cin >> input;
+    while(cin.fail()) {
+        cout << "Invalid input. Enter a number: ";
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> input;
+    }
+    while(input % 2 == 0) {
+        cout << "Invalid input. Enter an odd number: ";
+        cin >> input;
+    }
+    BoardRows = input;
+    cout << "Enter Columns: ";
+    cin >> input;
+    while(cin.fail()) {
+        cout << "Invalid input. Enter a number: ";
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> input;
+    }
+    while(input % 2 == 0) {
+        cout << "Invalid input. Enter an odd number: ";
+        cin >> input;
+    }
+    BoardColumns = input;
+    cout << "Enter the number of Zombie: ";
+    cin >> input;
+    while(cin.fail()) {
+        cout << "Invalid input. Enter a number: ";
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> input;
+    }
+    while(input % 2 == 0) {
+        cout << "Invalid input. Enter an odd number: ";
+        cin >> input;
+    }
+    Zombie = input;
+    cout << endl;
+    cout << "Settings Updated." << endl;
+    cout << "Press any key to continue." << endl;
+    cin.get(); 
+    return 0;
+}
+return 0;
 }
