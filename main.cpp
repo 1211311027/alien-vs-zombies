beff52e8452a63ebe8bec192c9f5a6a5663004f7
LEE RONG YI <123035643+1211311027@users.noreply.github.com>
2023-02-18 21:57:00 +0800

    add contact information

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+// *********************************************************
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS
+// Year: Trimester 1, 2022/23 (T2215)
+// Lab: TT8L
+// Names: LEE RONG YI | ABDULKAFI WALEED ABDULKAFI AL MALAMI | MOALI DOUAA
+// IDs: 1211311027 | 1201102665 | 1211307919
+// Emails: 1211311027@student.mmu.edu.my | 1201102665@student.mmu.edu.my |
+// 1211307919@student.mmu.edu.my
+// Phones: +60102736440 | +60183211296 | +60182565383
+// *********************************************************
+
 #include "pf/helper.h"
 #include <iostream>
 #include <vector>
